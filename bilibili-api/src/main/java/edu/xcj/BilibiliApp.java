1e577657fc068b651c2e4704d24c63f98cc04a54
xuecj <xuecjpersonal@163.com>
2022-05-22 22:46:35 +0800

    初始化项目，增加mysql链接和依赖

---
+++ bilibili-api/src/main/java/edu/xcj/BilibiliApp.java
@@ -0,0 +1,18 @@
+package edu.xcj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @author xuecj
+ * @version 1.0
+ * @date 2022/5/22 19:21
+ */
+@SpringBootApplication
+public class BilibiliApp {
+    public static void main(String[] args) {
+        ApplicationContext app = SpringApplication.run(BilibiliApp.class, args);
+
+    }
+}
