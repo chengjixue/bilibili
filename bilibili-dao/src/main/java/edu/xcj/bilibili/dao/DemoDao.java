8f9d1700b2df6876f675e04179658abd9bf81cbe
xuecj <xuecjpersonal@163.com>
2022-05-22 23:39:58 +0800

    2-8在项目中添加mybatis依赖，在配置文件中配置mybatis

---
+++ bilibili-dao/src/main/java/edu/xcj/bilibili/dao/DemoDao.java
@@ -0,0 +1,13 @@
+package edu.xcj.bilibili.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author xuecj
+ * @version 1.0
+ * @date 2022/5/22 23:33
+ */
+@Mapper
+public interface DemoDao {
+
+}
